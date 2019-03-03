e5dd245c62ae07131b74532c649854351ad77d4e
bjing2016 <bjing2016@users.noreply.github.com>
2019-03-03 01:32:03 -0800

    ready for first gefs download

--- get_gefs.py
+++ get_gefs.py
@@ -3,16 +3,8 @@ import pygrib
 import urllib.request
 import os.path
 import sys
-import h5py
-import time
 from datetime import datetime, timedelta
 
-###
-northlat = 60
-southlat = 0
-
-westlon = 180
-eastlon = 270
 
 
 levels = [10, 20, 30, 50, 70,\
@@ -34,7 +26,7 @@ def single_run(y,m,d,h,t,n, path):
     ## url = "ftp://ftp.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.20190302/00/pgrb2/gep01.t00z.pgrb2f12"
     base = datetime(y, m, d, h)
     basestring = base.strftime("%Y%m%d%H")    
-    pred = base + timedelta(hours=3)
+    pred = base + timedelta(hours=t)
 
     predstring = pred.strftime("%Y%m%d%H")
     savename = basestring + "_" + predstring + "_" + str(n).zfill(2)
@@ -50,7 +42,6 @@ def single_run(y,m,d,h,t,n, path):
     print("Saving " + savename)
 
     np.save(path + "/" + savename + ".npy", data)
-
     print("Deleting " + savename)
 
     os.remove(path + "/" + savename + ".grb2")
@@ -63,37 +54,20 @@ def grb2_to_array(filename):
     grbs = pygrib.open(filename + ".grb2")
     
     
-    dataset = np.zeros((2, len(levels), 10, 10))
+    dataset = np.zeros((2, len(levels), 180, 361))
 
     ### Thanks to KMarshland for pointers on using PyGrib ###
     for i in range(len(levels)):
         ### [lat, lat, lon, lon]
         for grb in grbs.select(shortName='u',typeOfLevel='isobaricInhPa', level = levels[i]):
-            print(time.time())
-            dataset[0][i] = grb.data(0,9,0,9)[0]
+            dataset[0][i] = grb.data()[0]
 
     for i in range(len(levels)):
         for grb in grbs.select(shortName='v',typeOfLevel='isobaricInhPa', level = levels[i]):
-            dataset[1][i] = grb.data(0,9,0,9)[0]
+            dataset[1][i] = grb.data()[0]
 
     return dataset
 
 
 
 
-
-def main():
-    y, m, d, h, path = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), sys.argv[5]
-    
-    timestamp = datetime(y, m, d, h)
-    base_timestamp = timestamp
-
-    base_string = timestamp.strftime("%Y%m%d%H")    
-    
-    t = 0
-    while t <= 384:
-        single_run(y, m, d, h, t, path)
-        t = t + 3
-
-
-###main()
