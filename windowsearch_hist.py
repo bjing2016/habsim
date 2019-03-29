ace7d0fcff534e22bdc63d8f2f4c26729d482245
bjing2016 <bjing2016@users.noreply.github.com>
2019-03-29 12:04:02 -0700

    cycloon historical search

--- windowsearch_hist.py
+++ windowsearch_hist.py
@@ -48,7 +48,7 @@ def main():
             print(name)
             cycloon_search(name, model_time, lat, lon, resultfile)
             resultfile.write("\n")
-        except IndexError:
+        except ValueError:
             print(name + " failed")
             continue
 
@@ -77,7 +77,7 @@ def cycloon_search(location_name, model_time, slat, slon, resultfile):
 
             sim_timestamp = launchtime.strftime("%Y%m%d%H")
             pathcache = list()
-            filename = location_name + sim_timestamp + "_" + str(rate);
+            filename = location_name + sim_timestamp + "_" + str(rate)
 
 
             resultfile.write("\n" + sim_timestamp + ": ")
