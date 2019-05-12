968a573a1e0891990cdd5d594242260318d6df82
bjing2016 <bjing2016@users.noreply.github.com>
2019-05-11 22:00:00 -0700

    Artificially start with May 11 0600 run per Jason's request

--- downloaderd.py
+++ downloaderd.py
@@ -20,6 +20,11 @@ def main():
     except FileExistsError:
         pass
     
+    ''' Special case to verify May 11 Launch '''
+    timestamp = datetime(2019, 5, 11, 6)
+    command = "python3 downloader.py " + str(timestamp.year) + " " + str(timestamp.month) + " " + str(timestamp.day) + " " + str(timestamp.hour)
+    os.system(command)
+    ''' End special case '''        
     now = datetime.utcnow()
     timestamp = datetime(now.year, now.month, now.day, int(now.hour / 6) * 6) - timedelta(hours=6)
     while True:
