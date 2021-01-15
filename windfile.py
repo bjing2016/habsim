742e0c8e76dc29a18581d11d81d6df6a435d251a
Richard Chen <richchen@stanford.edu>
2021-01-15 05:14:40 +0000

    cleaned up windfile

--- windfile.py
+++ windfile.py
@@ -1,62 +1,68 @@
 import numpy as np
 import math
 import elev
-from datetime import datetime, timedelta, timezone
+import datetime
 import bisect
 import time
+#from scipy import interpolate
 
 class WindFile:
-    def __init__(self, path: str):
+    def __init__(self, path):
         self.data_npz = np.load("downtest2/" + path, "r")
         self.data = self.data_npz['data']
         self.time = self.data_npz['timestamp']
-        self.altitude = self.data_npz['levels']
-        self.interval = self.data_npz['interval']//3600
+        self.levels = self.data_npz['levels']
+        self.interval = self.data_npz['interval']
         # TODO calculate resolution
 
-    def get(self, lat: float, lon: float, altitude: float, time: float):
-        if lat < -90 or lat > 90 or lon < -180 or lon > 360 or altitude > 975: # TODO cannot know altitude
-            raise Exception("Invalid input value") # TODO more precise (which axis)
-
+    def get(self, lat, lon, altitude, time):
+        if lat < -90 or lat > 90:
+            raise Exception("Latitude out of bounds")
+        if lon < -180 or lon > 360:
+            raise Exception("Longitude out of bounds")
+        
         # TODO wrap longitude
 
-        bounds = self.get_bounds_and_fractions(lat, lon, altitude, time)
+        indices = self.get_indices(lat, lon, altitude, time)
         
-        #diffs = GEFS_ALT_DIFFS if levels == GEFS else GFSHIST_ALT_DIFFS # ???? not sure if this is still needed
-        
-        u, v = self.get_wind_helper(*bounds)
+        u, v = self.interpolate(*indices)
         
         return u, v
 
-    def get_bounds_and_fractions(self, lat: float, lon: float, alt: float, time: float):
-        lat_res, lon_res, pressure_res = None, None, None
-        
+    def get_indices(self, lat, lon, alt, time):
         lat = 90 - lat
         lon = lon % 360
 
-        base_time = int(datetime.fromtimestamp(self.time).strftime("%Y%m%d%H"))
-        time = (time - base_time)/self.interval
+        if isinstance(time, datetime.date):
+            time = int(datetime.timestamp(time))
+        
+        time = (time - self.time)/self.interval
+        if time < 0 or time > 65: # should this be hard coded or like len(self.data[][])?
+            raise Exception("Time our of bounds")
 
-        pressure = self.get_pressure_bounds(alt)
+        pressure = self.get_pressure_index(alt)
         
         print("bounds:", lat, lon, pressure, time)
         return lat, lon, pressure, time
 
-    def get_pressure_bounds(self, alt: float):
-        pressure = self.alt_to_hpa(alt)
-        pressure_i = bisect.bisect_left(self.altitude, pressure)
-        if pressure_i == len(self.altitude):
-            return pressure_i-2
-        if pressure_i == 0:
-            return 0
-        return pressure_i-(self.altitude[pressure_i]-pressure)/float(self.altitude[pressure_i] - self.altitude[pressure_i-1])
+    def get_pressure_index(self, alt):
 
-    def get_wind_helper(self, lat: float, lon: float, level: float, time: float):
-        lat = int(lat)
-        lon = int(lon)
-        level = int(level)
-        time = int(time)
-        
+        # IGNORE: code for scipy interpolation
+        #x = np.arange(range(len(self.levels)))
+        #y = self.levels
+        #f = interpolate.interp1d(y, x)
+
+        pressure = self.alt_to_hpa(alt)
+
+        if pressure < self.levels[0]:
+            raise Exception("Pressure out of bounds")
+
+        pressure_i = bisect.bisect_left(self.levels, pressure)
+        if pressure_i == len(self.levels):
+            return pressure_i-1
+        return pressure_i-(self.levels[pressure_i]-pressure)/float(self.levels[pressure_i] - self.levels[pressure_i-1])
+
+    def interpolate(self, lat, lon, level, time):
         pressure_filter = np.array([1-level % 1, level % 1]).reshape(1, 1, 2, 1, 1)
         time_filter = np.array([1-time % 1, time % 1]).reshape(1, 1, 1, 2, 1) 
         lat_filter = np.array([1-lat % 1, lat % 1]).reshape(2, 1, 1, 1, 1)
@@ -66,6 +72,11 @@ class WindFile:
         print("time_filt", time_filter)
         print("lat_filt", lat_filter)
         print("lon_filt", lon_filter)
+
+        lat = int(lat)
+        lon = int(lon)
+        level = int(level)
+        time = int(time)
 
         cube = self.data[lat:lat+2, lon:lon+2, level:level+2, time:time+2, :]
        
@@ -80,7 +91,7 @@ class WindFile:
 
         return u, v
 
-    def alt_to_hpa(self, altitude): # TODO if < 1, then exception, not in this function
+    def alt_to_hpa(self, altitude):
         pa_to_hpa = 1.0/100.0
         if altitude < 11000:
             return pa_to_hpa * (1-altitude/44330.7)**5.2558 * 101325
@@ -96,7 +107,7 @@ class WindFile:
 def main():
     wind = WindFile("2021010100_01.npz")
     print(wind.time)
-    print(wind.get(45, 90, 350, 2021010106))
+    print(wind.get(45, 90, 350, 1609480800))
 
     validation()
 
